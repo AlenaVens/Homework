f5b9c8e889085598eac8404c912385fffa631ed2
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-09-27 10:41:45 +0200

    Add files via upload

--- homework2.java
+++ homework2.java
@@ -2,15 +2,20 @@ package homework;
 
 public class homework2 {
     public static void main(String[] args) {
-        int n =3;
-        int k= 14;
+        //задание1
+        int a = 179;
+        int b = a + 1;
+        int c = a - 1;
+        System.out.println("The next number for the number 179  is " + (b));
+        System.out.println("The previous number for the number 179 is " + (c));
+
+        //задание 2
+
+        int n = 3;
+        int k = 14;
         System.out.println(n);
         System.out.println(k);
         System.out.println(k % n);
-
-
-
-
 
 
     }
