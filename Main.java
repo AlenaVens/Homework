629984f3d47d6457db772955c05d424a2ef67620
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-09-27 10:32:18 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+        int a= 179;
+        int b = a+1;
+        int c = a-1;
+        System.out.println("The next number for the number 179  is " + (b));
+        System.out.println("The previous number for the number 179 is " + (c));
+
+    }
+}
---
+++ homework2.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class homework2 {
+    public static void main(String[] args) {
+        int n =3;
+        int k= 14;
+        System.out.println(n);
+        System.out.println(k);
+        System.out.println(k % n);
+
+
+
+
+
+
+    }
+}
