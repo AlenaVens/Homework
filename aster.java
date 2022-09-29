0384c4539f0072c27d5ad7a3de1510da2beb94c9
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-09-29 19:40:16 +0200

    Add files via upload

---
+++ aster.java
@@ -0,0 +1,21 @@
+package homework;
+
+public class aster {
+    public static void main(String[] args) {
+        //задача 1
+        long sekund = 3011456_567L;
+        long age = sekund / 365 / 24 / 60 / 60;
+        System.out.println(age + (" лет"));
+
+
+    // задача 2
+        boolean obeliks = false;
+        boolean asteriks = true;
+        int hour = 8;
+        boolean finishtime = (hour >5);
+        boolean antwort = finishtime || obeliks;
+        System.out.println(antwort);
+
+
+                }
+                }
