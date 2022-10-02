9b023ea464434988f85125151afb945a4001c3fe
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-10-02 17:33:40 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ package homework;
 
 public class Main {
     public static void main(String[] args) {
-        int a= 179;
-        int b = a+1;
-        int c = a-1;
-        System.out.println("The next number for the number 179  is " + (b));
-        System.out.println("The previous number for the number 179 is " + (c));
-
+        boolean cold = false;
+        boolean hot = true;
+        int air_temperature = 30;
+        int water_temperature = 28;
+        boolean answer = (air_temperature > 28 & water_temperature > 26);
+        System.out.println("Петя едет на море -" + answer);
     }
 }
