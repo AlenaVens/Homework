dc759638307b9677ae0b5d42126579054443f77d
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-10-02 18:03:43 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,11 +2,20 @@ package homework;
 
 public class Main {
     public static void main(String[] args) {
+        //задание1
+        int a = 6;
+        int b = 5;
+        int c = 8;
+        double answer = (a + b + c) / 3f;
+        System.out.println("Средний балл " + answer);
+
+        //задание 2
+
         boolean cold = false;
         boolean hot = true;
         int air_temperature = 30;
         int water_temperature = 28;
-        boolean answer = (air_temperature > 28 & water_temperature > 26);
-        System.out.println("Петя едет на море -" + answer);
+        boolean decision = (air_temperature > 28 & water_temperature > 26);
+        System.out.println("Петя едет на море -" + decision);
     }
 }
