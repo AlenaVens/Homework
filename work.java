2eccbfb7cf887601d31609ec22d1e2325ed80ac8
AlenaVens <114439623+AlenaVens@users.noreply.github.com>
2022-10-14 10:34:40 +0200

    Add files via upload

--- work.java
+++ work.java
@@ -1,13 +1,19 @@
 package homework;
 
+import java.util.Scanner;
+
 public class work {
     public static void main(String[] args) {
-        System.out.println("Я умею выводить строчки ");
-        System.out.println("Я начал программировать ");
-        System.out.println( 67 / 4 );
-        System.out.println(45.0 / 2 );
-        System.out.println(1234 * 1234);
-        System.out.println(123456 % 10);
-
-}
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int length = s.length();
+        char last = s.charAt((length) - 1);
+        if (length < 6 | last !='!')
+        {
+            System.out.println("Ваш пароль неверный");
+        }
+        else{
+            System.out.println("Пароль принят");
+        }
+    }
 }
